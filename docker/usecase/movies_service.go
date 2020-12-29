326248b391b405ce0ba819c5e0e0bf7d428507ff
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-12-29 01:13:58 -0400

    add: documentation

--- docker/usecase/movies_service.go
+++ docker/usecase/movies_service.go
@@ -7,20 +7,24 @@ import (
 	"github.com/thalysonalexr/learning-go/docker/infra/repo"
 )
 
+// Service interface of usecase
 type Service interface {
 	SearchByGender(k string) ([]entity.Movie, error)
 }
 
+// ServiceImpl is implementation of Service interface
 type ServiceImpl struct {
 	repo repo.Repository
 }
 
+// CreateNewService is a factory to create new Service
 func CreateNewService(r repo.Repository) *ServiceImpl {
 	return &ServiceImpl{
 		repo: r,
 	}
 }
 
+// SearchByGender is a handler to get list movies
 func (service *ServiceImpl) SearchByGender(k string) ([]entity.Movie, error) {
 	movies, err := service.repo.List()
 	if err != nil {
